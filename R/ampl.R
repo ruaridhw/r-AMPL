04d360324a5a71b730f6dfb696b3a012957d71eb
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2018-01-22 14:52:05 +0000

    Add documentation

--- R/ampl.R
+++ R/ampl.R
@@ -1,23 +1,96 @@
+#' Set Option
+#'
+#' Set an AMPL option to a specified value.
+#'
+#' @inheritParams read
+#' @param name Character Name of the option to be set (alphanumeric without spaces)
+#' @param value Character The value the option must be set to.
+#'
 #' @export
-set_option <- function(ampl, solver, solver_path) {
-  call_python(ampl, "setOption", solver, solver_path)
+set_option <- function(ampl, name, value) {
+  call_python(ampl, "setOption", name, value)
 }
 
+#' Set Solver
+#'
+#' Set the current model solver.
+#'
+#' @inheritParams read
+#' @param solver_path Character Path to the solver executable
+#'
+#' @export
+set_solver <- function(ampl, solver_path) {
+  set_option(ampl, "solver", solver_path)
+}
+
+#' Read
+#'
+#' Interprets the specified file.
+#' As a side effect, it invalidates all entities
+#' (as the passed file can contain any arbitrary command);
+#' the lists of entities will be re-populated lazily
+#' (at first access).
+#'
+#' `read_model` accepts a script or model or mixed.
+#'
+#' `read_data` accepts an AMPL data file. After reading
+#' the file the interpreter is put back to "model" mode.
+#'
+#' @param ampl `amplpy.ampl.AMPL` object
+#' @param fileName Full path to the file.
+#'
+#'
+#' @return amplpy.ampl.AMPL object
+#'
+#' @name read
 #' @export
 read_model <- function(ampl, fileName) {
   call_python(ampl, "read", fileName)
 }
 
+#' @rdname read
 #' @export
 read_data <- function(ampl, fileName) {
   call_python(ampl, "readData", fileName)
 }
 
+
+#' Evaluate
+#'
+#' Parses AMPL code and evaluates it as a possibly empty sequence of AMPL
+#' declarations and statements.
+#'
+#' As a side effect, it invalidates all entities (as the passed statements
+#' can contain any arbitrary command); the lists of entities will be
+#' re-populated lazily (at first access).
+#'
+#' The output of interpreting the statements is passed to the current
+#' OutputHandler (see getOutputHandler and setOutputHandler).
+#'
+#' By default, errors are reported as exceptions and warnings are printed
+#' on stdout. This behavior can be changed reassigning an ErrorHandler
+#' using setErrorHandler.
+#'
+#' @inheritParams read
+#' @param amplstatements A collection of AMPL statements and declarations to
+#' be passed to the interpreter.
+#'
 #' @export
 evaluate <- function(ampl, amplstatements) {
   call_python(ampl, "eval", amplstatements)
 }
 
+#' Use Objective
+#'
+#' Sets an objective function as the current model context
+#'
+#' Call this function if your model has multiple objectives defined in
+#' order to switch between them. This only alters the state of the model
+#' environment and doesn't execute the solver.
+#'
+#' @inheritParams read
+#' @param objective Character name of the objective function to be solved
+#'
 #' @export
 use_objective <- function(ampl, objective) {
   evaluate(ampl, paste0('objective ', objective, ';'))
--- R/amplpy.R
+++ R/amplpy.R
@@ -1,3 +1,15 @@
+#' AMPL Environment
+#'
+#' Creates a new AMPL model
+#'
+#' As this function instantiates a new background AMPL
+#' process, if you only require one model environment at
+#' a time, use `reset` to clear the current process
+#' instead.
+#'
+#' @param amplpy `python.builtin.module` AMPLPY module
+#' @param ampl_path Character Path to AMPL executable
+#'
 #' @importFrom reticulate import
 #' @export
 new_ampl_env <- function(amplpy = import("amplpy"),
