fe937c18955af4c48e37916814e2e99412fa997b
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2018-01-26 18:06:43 +0000

    Document

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,18 @@
+#' @param python_module python.builtin.module Object
+#' @param attribute Attribute/Method etc of `python_module`
+#' @param ... Arguments passed to `attribute`
+#'
+#' @noRd
 call_python <- function(python_module, attribute, ...) {
   do.call(python_module[[attribute]], list(...))
 }
 
+#' @param lookup List with all elements length 1
+#' @param x Vector with elements as the values of `lookup`
+#'
+#' @return Vector with elements as the names of `lookup`
+#' corresponding to each value of `x`
+#' @noRd
 reverse_list_lookup <- function(lookup, x) {
   names(lookup)[match(x, unlist(lookup))]
 }
